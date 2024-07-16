e36dfb9797d997ed496a8f004d88965cd2d9c65c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-16 10:27:07 -0400

    Pass all tests

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+  bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 10, 'M', 'brown', 'calm', true),
+  ('Rowdy', 5, 'M', 'black', 'aggressive', false),
+  ('Tabitha', 7, 'F', 'brown', 'calm', false),
+  (
+    'Sergeant Brown',
+    8,
+    'M',
+    'brown',
+    'protective',
+    true
+  ),
+  ('Melissa', 4, 'F', 'black', 'calm', true),
+  ('Grinch', 6, 'M', 'brown', 'grumpy', false),
+  ('Wendy', 3, 'F', 'white', 'friendly', true),
+  (NULL, 2, 'F', 'brown', 'unknown', true);
